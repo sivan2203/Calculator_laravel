5fbfd91922f5697350c08af82071f5f1ba8405f7
Bernard Tere <44svn@192.168.0.4>
2021-03-26 01:12:47 +0700

    create calculator form

---
+++ resources/views/index.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Байки из склепа</title>
+     <link rel="stylesheet" href="/css/styles.css">
+ </head>
+ <body>
+  <div id="header"><h1>Байки из склепа</h1></div>
+  <div id="sidebar">
+    <p><a href="b_all.html">Все байки</a></p>
+    <p><a href="b_author.html">Байки по автору</a></p>
+    <p><a href="b_theme.html">Байки по теме</a></p>
+    <p><a href="b_popular.html">Популярные байки</a></p>
+    <p><a href="b_last.html">Последние байки</a></p>
+  </div>
+  <div id="content">
+    <h2>Калькулятор демона </h2>
+    <form action='/'>
+        <p>Первое значение: <input type="text" value="0" id="num1"></p>
+        <p>Действие: <input type="text" value="0" id="sign"></p>
+        <p>Второе значение: <input type="text" value="0" id="num2"></p>
+        <p><input type="submit" value="Расчитать"></p>
+        <p id="answer">Ответ: 22</p>
+
+
+    </form>
+  </div>
+  <div id="footer">&copy; Иван Сысоев</div>
+ </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', MainController@index'');
+Route::get('/', 'MainController@index');
