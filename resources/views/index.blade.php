8e1d2d3e6c971180a6bdea380ca2d6bfaafef41d
Bernard Tere <44svn@192.168.0.4>
2021-03-26 22:36:38 +0700

    created ajax.js

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,6 +17,7 @@
   <div id="content">
     <h2>Калькулятор демона </h2>
       <form method="post" id="ajax_form" action="{{route('postData')}}">
+          @csrf
           <input type="text" name="num1" placeholder="Значение 1"><br>
           <input type="text" name="sign" placeholder="Действие"><br>
           <input type="text" name="num2" placeholder="Значение 2"><br>
@@ -26,6 +27,7 @@
   </div>
   <div id="footer">&copy; Иван Сысоев</div>
   <script src="/js/ajax.js"></script>
-  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+{{--  <script src="/js/jquery.min.js" type="script"></script>--}}
+  <script   src="https://code.jquery.com/jquery-3.6.0.js"   integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="   crossorigin="anonymous"></script>
  </body>
 </html>
