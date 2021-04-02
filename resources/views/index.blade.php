730ac6a29d3ddf50a0d6c5c8612ef5df6812c51c
sivan2203 <sivan9458@gmail.com>
2021-04-02 22:59:02 +0700

    add directory images and connect bootstrap

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,19 @@
         <meta charset="utf-8">
         <title>Байки из склепа</title>
         <link rel="stylesheet" href="/css/styles.css">
+        <script   src="/js/jquery.min.js"></script>
+        <script src="/js/ajax.js"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js" integrity="sha384-LtrjvnR4Twt/qOuYxE721u19sVFLVSA4hf/rRt6PrZTmiPltdZcI7q7PXQBYTKyf" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
+        <script type="text/javascript" src="/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+        <script type="text/javascript" src="/fancybox/jquery.easing-1.4.pack.js"></script>
+        <script type="text/javascript" src="/fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
     </head>
     <body>
     <div id="header"><h1>Байки из склепа</h1></div>
+    <div class="container"></div>
     <div id="sidebar">
         <p><a href="b_all.html">Все байки</a></p>
         <p><a href="b_author.html">Байки по автору</a></p>
@@ -26,7 +36,5 @@
         <div id="result_form"></div>
     </div>
     <div id="footer">&copy; Иван Сысоев</div>
-    <script   src="/js/jquery.min.js"></script>
-    <script src="/js/ajax.js"></script>
     </body>
 </html>
