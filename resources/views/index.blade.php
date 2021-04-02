3b54125a350e9bcdec493c5b80fabdf071cba791
sivan2203 <sivan9458@gmail.com>
2021-04-02 23:10:33 +0700

    teleport directory images in public and add 3 images

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,7 @@
         <link rel="stylesheet" href="/css/styles.css">
         <script   src="/js/jquery.min.js"></script>
         <script src="/js/ajax.js"></script>
+        <script src="/js/fancybox.js"></script>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js" integrity="sha384-LtrjvnR4Twt/qOuYxE721u19sVFLVSA4hf/rRt6PrZTmiPltdZcI7q7PXQBYTKyf" crossorigin="anonymous"></script>
@@ -13,6 +14,7 @@
         <script type="text/javascript" src="/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
         <script type="text/javascript" src="/fancybox/jquery.easing-1.4.pack.js"></script>
         <script type="text/javascript" src="/fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
+        <link rel="stylesheet" href="/fancybox/jquery.fancybox-1.3.4.css" type="text/css" media="screen" />
     </head>
     <body>
     <div id="header"><h1>Байки из склепа</h1></div>
