940b6ca96c770d1b5e628f3d9016d99be89d847d
sivan2203 <sivan9458@gmail.com>
2021-04-03 14:39:57 +0700

    Add gallery in page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,21 +10,21 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js" integrity="sha384-LtrjvnR4Twt/qOuYxE721u19sVFLVSA4hf/rRt6PrZTmiPltdZcI7q7PXQBYTKyf" crossorigin="anonymous"></script>
-        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
-        <script type="text/javascript" src="/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
-        <script type="text/javascript" src="/fancybox/jquery.easing-1.4.pack.js"></script>
-        <script type="text/javascript" src="/fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
-        <link rel="stylesheet" href="/fancybox/jquery.fancybox-1.3.4.css" type="text/css" media="screen" />
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
+        <script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
     </head>
     <body>
     <div id="header"><h1>Байки из склепа</h1></div>
-    <div class="container"></div>
+    <div class="container">
+            <a data-fancybox="gallery" href="/images/tech1big.jpeg"><img src="/images/tech1small.jpeg"></a>
+            <a data-fancybox="gallery" href="/images/tech2big.jpeg"><img src="/images/tech2small.jpeg"></a>
+            <a data-fancybox="gallery" href="/images/tech3big.jpeg"><img src="/images/tech3small.jpeg"></a>
+    </div>
     <div id="sidebar">
-        <p><a href="b_all.html">Все байки</a></p>
-        <p><a href="b_author.html">Байки по автору</a></p>
-        <p><a href="b_theme.html">Байки по теме</a></p>
-        <p><a href="b_popular.html">Популярные байки</a></p>
-        <p><a href="b_last.html">Последние байки</a></p>
+        <p><a href="b_all.html">Калькулятор</a></p>
+        <p><a href="b_author.html">Галерея</a></p>
+
     </div>
     <div id="content">
         <h2>Калькулятор демона </h2>
