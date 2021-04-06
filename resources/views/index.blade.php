b304a414e6c78a72aeff2f3410105f1dd90dd69b
sivan2203 <sivan9458@gmail.com>
2021-04-06 17:53:33 +0700

    install vue.js

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
         <script   src="/js/jquery.min.js"></script>
         <script src="/js/vue.js"></script>
         <script src="/js/ajax.js"></script>
-        <script src="/js/fancybox.js"></script>
+{{--        <script src="/js/fancybox.js"></script>--}}
 {{--        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">--}}
 {{--        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>--}}
 {{--        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js" integrity="sha384-LtrjvnR4Twt/qOuYxE721u19sVFLVSA4hf/rRt6PrZTmiPltdZcI7q7PXQBYTKyf" crossorigin="anonymous"></script>--}}
@@ -23,6 +23,10 @@
     </div>
     <div id="content">
         <h2>Калькулятор демона </h2>
+        <script src="js/app.js"></script>
+        <example-component></example-component>
+        <!-- need to put it before JQuery to let vuejs works -->
+
         <form method="post" id="form-data" data-route="{{route('postData')}}">
             @csrf
             <input type="text" name="num1" placeholder="Значение 1"><br>
